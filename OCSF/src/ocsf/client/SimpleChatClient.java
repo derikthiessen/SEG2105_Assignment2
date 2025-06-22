43ba2b5155713fd2dbaa9efd613bb07b230ec5fc
derikthiessen <derikt03@live.com>
2025-06-22 16:14:02 -0400

    new: main method for accepting host and port

--- OCSF/src/ocsf/client/SimpleChatClient.java
+++ OCSF/src/ocsf/client/SimpleChatClient.java
@@ -1,5 +1,7 @@
 package ocsf.client;
 
+import java.io.IOException;
+
 public class SimpleChatClient extends AbstractClient {
     
     /**
@@ -28,5 +30,30 @@ public class SimpleChatClient extends AbstractClient {
         System.out.println("Connection error: server has shut down or an exception occured when connecting.");
 		System.out.println(exception.getMessage());
 		System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        String host = "defaulthost";
+        int port = 5555;
+
+        if (args.length > 0) {
+            host = args[0];
+        }
+
+        if (args.length > 1) {
+            try {
+                port = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port number. Using default port 5555.");
+            }
+        }
+
+        SimpleChatClient client = new SimpleChatClient(host, port);
+        try {
+            client.openConnection();
+        } catch (IOException e) {
+            System.out.println("Could not connect to server at " + host + ":" + port);
+            System.exit(1);
+        }
     }
 }
